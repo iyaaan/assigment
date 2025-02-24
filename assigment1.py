a2a81e0fd78a9febb179b9600da78f1a8023f214
Wiliiiiiiiiiiiiiiiiiiiiii <william.michael_ti24@nusaputra.ac.id>
2025-02-24 11:04:27 +0700

    first commit

--- assigment1.py
+++ assigment1.py
@@ -8,4 +8,4 @@ a = int(input("Input the number for C: "))
 
 condition_1 = a > b or b > c
 condition_2 = a % 2 == 0 and b % 2 !=0
-condition_3 = 
+condition_3 =
--- assignment11.py
+++ assignment11.py
@@ -9,4 +9,3 @@ a = int(input("Enter value for a: "))
 b = int(input("Enter value for b: "))
 c = int(input("Enter value for c: "))
 check_conditions(a, b, c)
-1
